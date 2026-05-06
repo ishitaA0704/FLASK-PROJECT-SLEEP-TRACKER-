5441c28fbcc289d74025a877cab44e1b70f3998d
ishitaA0704 <ishitaagarwal3004@gmail.com>
2026-05-06 21:34:54 +0530

    Adding authentication

--- track/routes.py
+++ track/routes.py
@@ -1,5 +1,5 @@
 from track import app
-from flask import render_template, request, redirect, url_for
+from flask import flash, render_template, request, redirect, url_for
 from track import db
 from track.models import Sleep
 from track.log import LoginForm
@@ -10,10 +10,31 @@ from track.log import LoginForm
 def home():
     return render_template("home.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def Login():
     form = LoginForm()
-    return render_template("Login.html",form=form)
+
+    if form.validate_on_submit():
+
+        # check if user already exists
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user:
+            flash("User already exists!", category="danger")
+        else:
+            new_user = User(
+                name=form.name.data,
+                email=form.email.data,
+                password=form.password1.data
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash("Account created successfully!", category="success")
+            return redirect(url_for('home'))
+
+    return render_template("Login.html", form=form)
 
 @app.route("/track", methods=["GET", "POST"])
 def Track():
